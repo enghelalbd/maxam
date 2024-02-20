9c2eefe6a8a57927771dd9c41a5f3a8405661460
enghelalbd <53162411+enghelalbd@users.noreply.github.com>
2024-02-20 12:00:55 +0600

    Add files via upload

---
+++ higher.cpp
@@ -0,0 +1,14 @@
+#include<iosteam>
+using namespace std;
+int main(){
+    int a=3,9;
+    cin<<a<<b<<endl;
+    if(a<b){
+        cout<<b<<endl;
+    }
+    else{
+        cout<<a<<endl;
+    }
+
+return 0;
+}
